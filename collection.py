457b56478497c83bd2e1a392e8bc07ffeea64a35
Rishidh-VyomA <hrishikeshdeeraj@gmail.com>
2024-01-18 10:27:22 +0530

    Some quick fixes

--- collection.py
+++ collection.py
@@ -1,11 +1,15 @@
 name = input("Hello and welcome! Please enter your name: ")
 print(f"Hello, {name}!")
 
-# Data types are: String, Integer, Float, Boolean
+# Data types are:
+# String
+# Integer
+# Float
+# Boolean
 
 # Type conversion:
 
 name = input("Please enter your name: ")
-birth_year = input(int("Now your age: "))
+birth_year = int(input("Now your birth year please: "))
 age = 2023 - birth_year
 print(f"{name}, you are {age} years old")
