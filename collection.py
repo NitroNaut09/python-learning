4140fd0570a348e09f418afbc7e5620896062251
Rishidh-VyomA <hrishikeshdeeraj@gmail.com>
2024-02-06 09:47:13 +0530

    Some MOds

--- collection.py
+++ collection.py
@@ -62,3 +62,39 @@ print(10/3)   #Division
 print(10**3)  #Exponentiation
 print(10%3)   #Shows the remainder after performing dvision.
 print(10//3)  #Floor division, returns integer value.
+
+#  Assignment Operator
+
+a = 10
+a += 3 #Adds the value of "3" to the variable "a"
+a -= 3 #Subtracts the value of "3" from the variable "a"
+
+#  Comparison Operator
+
+a == b #Equal to
+a != b #Not equal to
+a > b  #Greater than
+a < b  #Less than
+a >= b #Greater than or equal to
+a <= b #Less than or equal to
+
+#  Membership operator:
+x in y        #Is x a member of y?
+x not in y    #Is x not a member of y?
+
+#  Operator Precendence
+
+** (Exponentiation)
++x, -x (Positive, Negative)
+*, /, //, % (Multiplication, Division, Floor Division, Modulus)
++, - (Addition, Subtraction)
+<<, >> (Bitwise Shifts)
+& (Bitwise AND)
+^ (Bitwise XOR)
+| (Bitwise OR)
+in, not in, is, is not, <, <=, >, >=, !=, == (Comparisons, including membership tests and identity tests)
+not x (Boolean NOT)
+and (Boolean AND)
+or (Boolean OR)
+
+
