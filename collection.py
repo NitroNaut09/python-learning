3b4e16a1c247aea6a72799bc5d5d2260274a9fb5
Rishidh-VyomA <hrishikeshdeeraj@gmail.com>
2024-02-19 14:21:04 +0530

    Learned return statements, functions, and going to learn exception (Day 4)

--- collection.py
+++ collection.py
@@ -296,3 +296,36 @@ def greet_user(first_name, last_name):
     print(f'Hi {first_name} {last_name}!')
 
 greet_user(last_name = "Smith", first_name = "John")
+
+
+#Return statements:   
+
+def square(number):
+    return number * number
+print(square(3))
+
+#Things learnt: By default, all funtions in python return none.
+
+#Converting emoji program into a functions:
+
+def emoji_converter(message):
+    words = message.split(" ")
+    emojis = {
+        ":)": "😀",
+        ":(": "😔"
+    }
+    output = ""
+    for word in words:
+        output += emojis.get(word, word) + " "
+    return output
+
+
+message = input(">")
+print(emoji_converter(message))
+
+
+#Exceptions:
+
+age = int(input('Age: '))
+print(age)
+
